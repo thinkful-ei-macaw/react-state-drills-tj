5df11493f852d5028510ffe538a717b3801d7cb8
Trina <trinamceary@gmail.com>
2020-03-04 12:26:43 -0800

    BOOM

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,24 +1,40 @@
-import React, { Component } from 'react';
+
+import React, { Component } from 'react'
 
 export default class Bomb extends Component {
   state = {
-    count : 0,
+    count: 0,
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000)
   }
 
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
-handleButtonClick = () => {
- this.setState({ count: this.state.count + 1});
-}
-
-
-
+  renderDisplay() {
+    const { count } = this.state
+    if (count >= 8) {
+      clearInterval(this.interval)
+      return 'BOOM!!!!'
+    } else if (count % 2 === 0) {
+      return 'tick'
+    } else {
+      return 'tock'
+    }
+  }
 
   render() {
     return (
-      <div>
-        <p>Tick</p>
+      <div className='CountdownBomb'>
+        {this.renderDisplay()}
       </div>
-)
-}
-
+    )
+  }
 }
