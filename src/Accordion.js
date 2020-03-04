fe8c22deca9569d7fb454b744202c097b7fe9f1a
Trina <trinamceary@gmail.com>
2020-03-04 14:37:44 -0800

    typos

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-export default class Acordion extends Component {
+export default class Accordion extends Component {
 static defaultProps = {
   sections: [
     {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import RouletteGun from './state-drills/RouletteGun'
-import Acordion from './Acordion';
+import Accordion from './Accordion';
 
-ReactDOM.render(<Acordion />, document.getElementById('root'));
+ReactDOM.render(<Accordion />, document.getElementById('root'));
